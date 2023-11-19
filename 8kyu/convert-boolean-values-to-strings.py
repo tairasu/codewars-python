a0e11689cd3f690361dfd1f33e7dfab4a396b6ad
aomizu <marco.tylus24@gmail.com>
2023-11-19 14:43:13 +0100

    added codewars solutions

---
+++ 8kyu/convert-boolean-values-to-strings.py
@@ -0,0 +1,9 @@
+# Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+# https://www.codewars.com/kata/53369039d7ab3ac506000467/train/python/65591fbe219a8c39b63b5c11
+
+def bool_to_word(boolean):
+    if(boolean):
+        return 'Yes'
+    else:
+        return 'No'
---
+++ 8kyu/convert-number-to-a-string.py
@@ -0,0 +1,14 @@
+# We need a function that can transform a number (integer) into a string.
+
+# What ways of achieving this do you know?
+
+# Examples (input --> output):
+
+# 123  --> "123"
+# 999  --> "999"
+# -100 --> "-100"
+
+# https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/python/65591eb5928cf703db1ab4c6
+
+def number_to_string(num):
+    return str(num)
