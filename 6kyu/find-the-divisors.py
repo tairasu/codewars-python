daf20e3266e7381edf6e4444faba8308746a3185
aomizu <marco.tylus24@gmail.com>
2023-11-20 15:49:57 +0100

    added find the divisors

---
+++ 6kyu/find-the-divisors.py
@@ -0,0 +1,19 @@
+# Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#, empty table in COBOL) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+
+# Example:
+
+# divisors(12); #should return [2,3,4,6]
+# divisors(25); #should return [5]
+# divisors(13); #should return "13 is prime"
+
+# https://www.codewars.com/kata/544aed4c4a30184e960010f4/train/python
+
+def divisors(integer):
+    divisors = []
+    for i in range(2, integer):
+        if(integer%i==0):
+            divisors.append(i)
+    if(len(divisors)==0):
+        return str(integer) + " is prime"
+    else:
+        return divisors
